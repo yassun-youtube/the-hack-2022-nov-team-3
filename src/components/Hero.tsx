435232235c4802b6e5d84440087f3e062251fe06
underground0930 <go.nishiduka.1985@gmail.com>
2022-12-11 15:40:23 +0900

    styleのシンタックスエラーを修正、imgのaltを付加

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,34 +18,35 @@ const Hero: FC<Props> = ({ userName, mainHeroImage, profileThumbnailImage }: Pro
   return (
     <div
       css={css`
-      .relative {
-        position: relative;
-        margin-bottom:100px;
-      }
-      .hero_picture{
-        width:100%;
-      }
-      .hero_icon{
-        position: absolute;
-        bottom:25px;
-        left:140px;
-        z-index:10;
-      }
-      .absolute {
-        position: absolute;
-        width:150px;
-        height:100px;
-        bottom:-50px;
-        left:20px;
-        border: solid 3px #fff; 
-    `}
+        .relative {
+          position: relative;
+          margin-bottom: 100px;
+        }
+        .hero_picture {
+          width: 100%;
+        }
+        .hero_icon {
+          position: absolute;
+          bottom: 25px;
+          left: 140px;
+          z-index: 10;
+        }
+        .absolute {
+          position: absolute;
+          width: 150px;
+          height: 100px;
+          bottom: -50px;
+          left: 20px;
+          border: solid 3px #fff;
+        }
+      `}
     >
       <div className="relative">
-        <img src={mainHeroImage} className="hero_picture" />
+        <img src={mainHeroImage} className="hero_picture" alt="" />
         <Avatar className="hero_icon" sx={{ bgcolor: '' }}>
           {userName[0].toUpperCase()}
         </Avatar>
-        <img src={profileThumbnailImage} className="absolute" />
+        <img src={profileThumbnailImage} className="absolute" alt="" />
       </div>
     </div>
   )
