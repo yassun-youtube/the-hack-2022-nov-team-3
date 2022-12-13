3827e147469251070004289f68f923ba1f78013e
underground0930 <go.nishiduka.1985@gmail.com>
2022-12-13 17:53:37 +0900

    詳細ページのfetch処理を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,86 @@
 /** @jsxImportSource @emotion/react */
 'use client'
-import { useEffect } from 'react'
+import { useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import Container from '@mui/material/Container'
-
-import { client } from '~/libs'
 
 // components
 import { NormalButton, Title, Section } from '~/components'
 
-export default function Home() {
-  useEffect(() => {
-    client
-      .getContents({
-        appUid: 'members',
-        modelUid: 'member',
-      })
-      .then((content) => console.log(content))
-      .catch((err) => console.log(err))
-  }, [])
+// hooks
+import { useFetchMembers, useFetchCategories } from '~/hooks'
+
+export default function Page() {
+  const [useParams, setUseParams] = useState({
+    skip: 0,
+    limit: 100,
+  })
+  const {
+    data: membersData,
+    isError: membersIsError,
+    isLoading: membersIsLoading,
+    error: membersError,
+  } = useFetchMembers(useParams) // メンバーのデータを取得
+  const {
+    data: hobbyData,
+    isError: hobbyIsError,
+    isLoading: hobbyLoading,
+    error: hobbyError,
+  } = useFetchCategories({ category: 'hobby' }) // 趣味のデータを取得
+  const {
+    data: skillData,
+    isError: skillIsError,
+    isLoading: skillIsLoading,
+    error: skillError,
+  } = useFetchCategories({ category: 'skill' }) // 技術のデータを取得
+  const {
+    data: prefecturesData,
+    isError: prefecturesIsError,
+    isLoading: prefecturesIsLoading,
+    error: prefecturesError,
+  } = useFetchCategories({ category: 'prefectures' }) // 都道府県のデータを取得
 
   return (
     <>
       <CssBaseline />
       <Container maxWidth="lg">
-        {/* 絞り込み */}
         <Section>
           <Title text={'メンバーの絞り込み'} />
-          <NormalButton
-            variant="contained"
-            clickHandler={() => {
-              alert('reset')
-            }}
-          >
+          <div style={{ display: 'flex' }}>
+            <ul>
+              {skillData?.items && skillData?.items.map((v) => <li key={v.slug}>{v.label}</li>)}
+            </ul>
+            <ul>
+              {hobbyData?.items && hobbyData?.items.map((v) => <li key={v.slug}>{v.label}</li>)}
+            </ul>
+            <ul>
+              {prefecturesData?.items &&
+                prefecturesData?.items.map((v) => <li key={v.slug}>{v.label}</li>)}
+            </ul>
+          </div>
+          <NormalButton variant="contained" clickHandler={() => alert('reset')}>
             検索条件をリセット
           </NormalButton>
         </Section>
-        {/* 運営者 */}
-        <Section>
-          <Title text={'運営者'} />
-        </Section>
-        {/* サロンメンバー */}
-        <Section>
-          <Title text={'サロンメンバー'} />
-        </Section>
+        {membersIsLoading && <>...isLoading</>}
+        {membersIsError && <>エラーです</>}
+        {membersData?.items && membersData.items.length > 0 && (
+          <div>
+            <Section>
+              <Title text={'メンバー'} />
+              <div>
+                <ul>
+                  {membersData?.items &&
+                    membersData?.items.map((v) => (
+                      <li key={v.slug}>
+                        <a href={`/member/${v.slug}`}>{v.name}</a>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </Section>
+          </div>
+        )}
       </Container>
     </>
   )
